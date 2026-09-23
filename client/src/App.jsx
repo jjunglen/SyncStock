1720b15505451c618bb801224e5e79883566fde6
John Paul Junglen <j.junglen@gmail.com>
2026-09-23 07:23:51 -0500

    feat: refactor AccountSettings component and update import paths in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,19 @@ import Landing from "./pages/Landing.jsx";
 import PricingPage from "./pages/Pricing.jsx";
 import AuthForm from "./components/auth/AuthForm.jsx";
 import ConnectShopify from "./pages/onboarding/ConnectShopify.jsx";
-import OnboardingSteps from "./pages/onboarding/OnboardingSteps.jsx"
+import OnboardingSteps from "./pages/onboarding/OnboardingSteps.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
-import MerchantDashboard from "./components/dashboard/MerchantDashboard.jsx"
+import MerchantDashboard from "./components/dashboard/MerchantDashboard.jsx";
 import MarketingHeader from "./components/marketing/MarketingHeader.jsx";
 import SourcingPage from "./components/dashboard/SourcingPage.jsx";
 import PurchasesPage from "./pages/dashboard/PurchasesPage.jsx";
-import AccountSettings from "./components/dashboard/AccountSettings.jsx";
-import CustomersPage from "./components/dashboard/CustomerPage.jsx";
+import AccountSettings from "./pages/dashboard/AccountSettings.jsx";
+import CustomersPage from "./pages/dashboard/CustomersPage.jsx";
 import StoreLanding from "./pages/StoreLanding.jsx";
 import CustomerDashboard from "./pages/dashboard/CustomerDashboard.jsx";
 import TrackShoe from "./pages/dashboard/TrackShoe.jsx";
 import CustomerProfile from "./pages/dashboard/CustomerProfile.jsx";
 import CustomerAuthForm from "./pages/store/CustomerAuthForm.jsx";
-
 
 function App() {
   return (
@@ -35,7 +34,6 @@ function App() {
           <div className="pt-16"><AuthForm initialMode="signup" /></div>
         </div>
       } />
-      <Route path="/signup" element={<AuthForm initialMode="signup" />} />
       <Route path="/onboarding" element={<ConnectShopify />} />
       <Route path="/onboarding/subdomain" element={<OnboardingSteps />} />
       <Route path="/store" element={<StoreLanding />} />
@@ -45,8 +43,8 @@ function App() {
       <Route path="/account" element={<ProtectedRoute><AccountSettings /></ProtectedRoute>} />
       <Route path="/customers" element={<ProtectedRoute><CustomersPage /></ProtectedRoute>} />
       <Route path="/store/dashboard" element={<ProtectedRoute><CustomerDashboard /></ProtectedRoute>} />
-      <Route path="store/track" element={<ProtectedRoute><TrackShoe /></ProtectedRoute>} />
-      <Route path="store/profile" element={<ProtectedRoute><CustomerProfile /></ProtectedRoute>} />
+      <Route path="/store/track" element={<ProtectedRoute><TrackShoe /></ProtectedRoute>} />
+      <Route path="/store/profile" element={<ProtectedRoute><CustomerProfile /></ProtectedRoute>} />
       <Route path="/store/login" element={<CustomerAuthForm initialMode="login" />} />
       <Route path="/store/signup" element={<CustomerAuthForm initialMode="signup" />} />
     </Routes>
